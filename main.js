751db9824745e8917d0260559384c1c745ed2044
tmilne1 <tmilne1@gmail.com>
2019-12-29 14:49:17 -0500

    added success and error functions for "WatchPosition()"

--- main.js
+++ main.js
@@ -4,9 +4,29 @@ const meters = document.querySelector('.meters');
 const miles = document.querySelector('.miles');
 
 
-navigator.geolocation.watchPosition(data=>{
+/*navigator.geolocation.watchPosition(data=>{
 
     meters.textContent = Math.round(data.coords.speed)
     miles.textContent = Math.round(data.coords.speed * 0.621371)
     arrow.style.transform = `rotate(${data.coords.heading}deg)`
 })
+*/
+
+function success(data){
+    meters.textContent = Math.round(data.coords.speed)
+    miles.textContent = Math.round(data.coords.speed * 0.621371)
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
+}
+
+function err(err){
+    console.err(err);
+    alert('You have to enalbe GPS location for this to work')
+}
+
+navigator.geolocation.watchPosition(success, err)
+
+/*
+WebSocket network error: The operation couldn’t be completed. (OSStatus error -9807.) ==
+errSSLXCertChainInvalid — The peer has an invalid certificate chain; 
+for example, signature verification within the chain failed, or no certificates were found.
+*/

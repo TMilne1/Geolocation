f4a4c5239c9703322986791d413567200d2f3813
tmilne1 <tmilne1@gmail.com>
2019-12-29 14:41:37 -0500

    updated directional heading

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const miles = document.querySelector('.miles');
 
 
 navigator.geolocation.watchPosition(data=>{
-    console.log(data)
+
     meters.textContent = Math.round(data.coords.speed)
     miles.textContent = Math.round(data.coords.speed * 0.621371)
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`
 })
